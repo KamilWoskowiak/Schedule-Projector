bcdcacd09ebe7935a1e473138f049ff66bf72014
KamilWoskowiak <kamilwoskowiak1@gmail.com>
2025-01-13 21:51:25 -0500

    start

---
+++ server/src/main/java/com/scheduleProjector/scheduleProjector/ScheduleProjectorApplication.java
@@ -0,0 +1,13 @@
+package com.scheduleProjector.scheduleProjector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleProjectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleProjectorApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/scheduleProjector/scheduleProjector/ScheduleProjectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scheduleProjector.scheduleProjector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleProjectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
