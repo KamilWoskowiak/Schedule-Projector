55e96b4e28af3b4e15f0baf1852057d2da71d8d0
KamilWoskowiak <kamilwoskowiak1@gmail.com>
2025-01-17 03:54:57 -0500

    todo for tmmrw (finish this up)

---
+++ server/src/main/java/com/scheduleProjector/scheduleProjector/controller/ScheduleGenerator.java
@@ -0,0 +1,17 @@
+package com.scheduleProjector.scheduleProjector.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/schedule")
+@RestController
+public class ScheduleGenerator {
+
+    @PostMapping("/create")
+
+
+    @DeleteMapping("/delete")
+
+}
