a47c349f5138f22a71a08be92bdbedec174ac7c3
KamilWoskowiak <kamilwoskowiak1@gmail.com>
2025-01-27 23:57:45 -0500

    Implemented preffered semester data, now must complete get and delete mappings for the data.
    Todo:
    -topological sorting
    -finish getting/deleting data

--- server/src/main/java/com/scheduleProjector/scheduleProjector/controller/DataHandlerController.java
+++ server/src/main/java/com/scheduleProjector/scheduleProjector/controller/DataHandlerController.java
@@ -34,12 +34,37 @@ public class DataHandlerController {
     }
 
     @GetMapping()
-    public ResponseEntity<DataHandlerController> getData(
+    public ResponseEntity<?> getData(
             Authentication authentication
     ) {
         String email = (String) authentication.getPrincipal();
-        return ResponseEntity.ok(this);
+        try {
+            BulkDataDto data = dataHandlerService.getData(email);
+            return ResponseEntity.ok().body(data);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body("Provided an email that does not exist within our database.");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
+
+    @DeleteMapping()
+    public ResponseEntity<?> deleteData(
+            @RequestBody String[] data,
+            Authentication authentication
+    ) {
+        String email = (String) authentication.getPrincipal();
+        try {
+            dataHandlerService.deleteData(data, email);
+            return ResponseEntity.ok("Courses & semesters deleted successfully");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+    )
 
 
 }
